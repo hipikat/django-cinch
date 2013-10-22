f269671e09299dded072c87c4b041b6f301041f1
Adam Wright <adam@hipikat.org>
2013-10-22 09:47:44 +0800

    Heavy refactoring

--- cinch/__init__.py
+++ cinch/__init__.py
@@ -1,170 +1,195 @@
 
 from os import environ, path
 import sys
-from runpy import run_module
 from django.core.exceptions import ImproperlyConfigured
-from configurations import Configuration
-from .caches import *
-from .common import *
-from .databases import *
+#from configurations import Configuration
 
 
-def cinch_settings(globals_dict, name=None):
-    if not name:
-        name = 'prod' if not globals_dict.get('DEBUG') else 'debug'
-    module_name = 'cinch.settings.' + name
-    return run_module(module_name, globals_dict)
+def cinch_django_settings(settings_globals=None, env_var='DJANGO_SETTINGS_CLASS'):
+    settings_class = settings_globals[environ[env_var]]
+    settings = {att: getattr(settings_class, att)
+                for att in dir(settings_class())
+                if att == att.upper()}
+    settings_globals.update(settings)
 
 
-class BaseConfiguration(Configuration):
+class CinchSettings(object):
+    """
+    Base class for settings classes. TODOLipsum
+    """
 
-    @classmethod
-    def _get_cinch_meta(cls, value, *default):
-        """Return a named value from a configuration's CinchMeta class."""
+    # Names of attributes which have only been set with CinchSettings.setdefault()
+    _defaults = set()
+
+    def __init__(self, *args, **kwargs):
+        super(CinchSettings, self).__init__(*args, **kwargs)
+        self.setup()
+
+    def __setattribute__(self, name, value):
+        """
+        Set an attribute on the class, removing its name from the set of attributes
+        which have been attached to the class using CinchSettings.setdefault().
+        """
+        # If this attribute was previously set with setdefault(), we can safely
+        # discard it from the set of _defaults. If it hasn't, setdefault() will
+        # add it to that set in the extremely near future.
+        self._defaults.discard(name)
+        super(CinchSettings, self).__setattribute__(name, value)
+
+    def explicit(self, name):
+        """
+        Return True if the attribute ``name`` has been explicitely set on this
+        class, i.e. not with setdefault(). Otherwise, return False.
+        """
+        return True if hasattr(self, name) and name not in self._defaults else False
+
+    def setdefault(self, key, *default):
+        """
+        If ``key`` is an attribute of this instance, return its value. If not,
+        set the attribute ``key`` to ``default[0]`` and return its value. If a
+        default is not passed and ``key`` is not an attribute on the instance,
+        an ``AttributeError`` is raised.
+        """
+        if len(default) > 1:
+            raise TypeError("{}.setdefault takes 0 or 1 argument ({} given)".format(
+                self.__class__.__name__, len(default)))
         try:
-            return getattr(cls.CinchMeta, value)
+            return getattr(self, key)
         except AttributeError:
-            try:
+            if default:
+                setattr(self, key, default[0])
+                self._defaults.add(key)
                 return default[0]
-            except IndexError:
-                raise AttributeError(
-                    "Missing required attribute {}.CinchMeta.{}".format(cls.__name__, value))
-        
-    @classmethod
-    def setup(cls):
-        """Process any methods on this class with the name ``_setup__foo``."""
-        super(BaseConfiguration, cls).setup()
-        for name in dir(cls):
-            if name.startswith('_setup__'):
-                getattr(cls, name)()
-        # TODOLipsum: set defaults
-        if not hasattr(cls, 'ROOT_URLCONF'):
-            cls.ROOT_URLCONF = cls.PROJECT_NAME + '.urls'
-        #import pdb; pdb.set_trace()
+            raise
 
-    SITE_ID = 1
+    def setup(cnf, *args, **kwargs):
+        """Do nothing; CinchSettings should always be the final base class."""
 
-    # Debugging, development and testing
-    TESTING = True if 'test' in sys.argv else False
-    DEBUG = False   # Never assume o.o
-    @classmethod
-    def _setup__debug(cls):
-        # TODO: Can we use setdefault on cls.__dict__ or something?
-        if not hasattr(cls, 'TEMPLATE_DEBUG'):
-            cls.TEMPLATE_DEBUG = cls.DEBUG
 
-    INTERNAL_IPS = ('127.0.0.1',)
+class NormaliseSettings(object):
+    """
+    Mixin to set sensible defaults on a CinchSettings class. TODOLipsum
+    """
+    __missing_setting_msg = "Attribute {} must be set on a class instance "\
+        "which includes the NormaliseSettings mixin in its bases."
 
-    # URLs
-    STATIC_URL = '/static/'
-    MEDIA_URL = '/media/'
+    def setup(cnf, *args, **kwargs):
+        """Set default attributes on this class, if they haven't been set."""
 
-    # Databases
-    @classmethod
-    def _setup__databases(cls):
-        """
-        If ``DATABASES`` haven't been defined, configure a default database
-        based on DATABASE_TYPE, falling back to sqlite if it hasn't been set.
-        Valid values for ``DATABASE_TYPE`` are 'postgresql' and 'sqlite'.
-        """
-        if hasattr(cls, 'DATABASES') and len(cls.DATABASES):
-            return
+        # Project metadata
+        try:
+            cnf.setdefault('MANAGERS', cnf.ADMINS)
+        except AttributeError:
+            raise ImproperlyConfigured(cnf.__missing_setting_msg.format('ADMINS'))
+        cnf.setdefault('TIME_ZONE', 'UTC')      # Default is "America/Chicago"
+        cnf.setdefault('LANGUAGE_CODE', 'en')   # Default is 'en-us'
+        cnf.setdefault('SITE_ID', 1)            # Default is not defined
+        cnf.setdefault('WSGI_APPLICATION', cnf.PROJECT_MODULE + '.urls')
 
-        db_user = cls._get_cinch_meta('DATABASE_USER', cls.PROJECT_NAME)
-        db_type = cls._get_cinch_meta('DATABASE_TYPE', 'sqlite')
-        db_suffix = cls._get_cinch_meta('DATABASE_SUFFIX',
-            '_default' if db_type != 'sqlite' else '_default.db')
-        db_name = cls._get_cinch_meta('DATABASE_NAME', cls.PROJECT_NAME + db_suffix)
+        # Debugging, testing, development
+        cnf.setdefault('DEBUG', False)
+        cnf.setdefault('TEMPLATE_DEBUG', cnf.DEBUG)
+        cnf.setdefault('TESTING', True if 'test' in sys.argv else False)
 
-        cls.DATABASES = {}
-        if db_type is 'postgresql':
-            cls.DATABASES['default'] = {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': db_name,
-                'USER': db_user,
-            }   
+        # Security
+        cnf.setdefault('INTERNAL_IPS', tuple('127.0.0.1'))
 
-        elif db_type is 'sqlite':
-            cls.DATABASES['default'] = {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': path.join(cls.DB_DIR, db_name),
-            }
-        else:
-            raise ImproperlyConfigured("Unknown DATABASE_TYPE: {}".format(db_type))
+        # URLs
+        cnf.setdefault('STATIC_URL', '/static/')
+        cnf.setdefault('MEDIA_URL', '/media/')
+        if hasattr(cnf, 'PROJECT_MODULE'):
+            cnf.setdefault('ROOT_URLCONF', cnf.PROJECT_MODULE + '.urls')
+
+        # Logging
+        cnf.setdefault('LOGGING', {
+            'formatters': {
+                'verbose': {
+                    'format': "\n%(levelname)s [%(asctime)s][%(pathname)s:%(lineno)s]" +
+                              "[p/t:%(process)d/%(thread)d]\n%(message)s"
+                },
+                'simple': {
+                    'format': '%(levelname)s [%(module)s:%(lineno)s] %(message)s'
+                },
+            },
+            'filters': {
+                'require_debug_false': {
+                    '()': 'django.utils.log.RequireDebugFalse',
+                },
+            },
+            'handlers': {
+                'mail_admins': {
+                    'level': 'ERROR',
+                    'filters': ['require_debug_false'],
+                    'class': 'django.utils.log.AdminEmailHandler',
+                },
+            },
+            'loggers': {
+                'django.request': {
+                    'handlers': ['mail_admins'],
+                    'level': 'ERROR',
+                    'propagate': True,
+                },
+            },
+        })
+
+        super(NormaliseSettings, cnf).setup(*args, **kwargs)
+
+# Weak i18n :)
+NormalizeSettings = NormaliseSettings
 
 
 class FHSDirsMixin(object):
     @classmethod
-    def setup(cls):
-        cls.LIB_DIR = path.join(cls.BASE_DIR, 'lib')
-        cls.VAR_DIR = path.join(cls.BASE_DIR, 'var')
-        cls.ETC_DIR = path.join(cls.BASE_DIR, 'etc')
-        cls.SRC_DIR = path.join(cls.BASE_DIR, 'src')
-        cls.DB_DIR = path.join(cls.VAR_DIR, 'db')
-        cls.LOG_DIR = path.join(cls.VAR_DIR, 'log')
+    def setup(cnf, *args, **kwargs):
+        cnf.setdefault('ETC_DIR', path.join(cnf.BASE_DIR, 'etc'))               # etc/
+        cnf.setdefault('LIB_DIR', path.join(cnf.BASE_DIR, 'lib'))               # lib/
+        cnf.setdefault('SRC_DIR', path.join(cnf.BASE_DIR, 'src'))               # src/
+        cnf.setdefault('TEMPLATE_DIRS', [path.join(cnf.SRC_DIR, 'templates')])  # src/templates/
+        cnf.setdefault('VAR_DIR', path.join(cnf.BASE_DIR, 'var'))               # var/
+        cnf.setdefault('DB_DIR', path.join(cnf.VAR_DIR, 'db'))                  # var/db/
+        cnf.setdefault('FIXTURES_DIRS', [path.join(cnf.VAR_DIR, 'fixtures')])   # var/fixtures/
+        cnf.setdefault('LOG_DIR', path.join(cnf.VAR_DIR, 'log'))                # var/log/
+        cnf.setdefault('MEDIA_ROOT', path.join(cnf.VAR_DIR, 'media'))           # var/media/
+        cnf.setdefault('STATIC_ROOT', path.join(cnf.VAR_DIR, 'static'))         # var/static/
 
-        if not hasattr(cls, 'MEDIA_ROOT') or not len(cls.MEDIA_ROOT):
-            cls.MEDIA_ROOT = path.join(cls.VAR_DIR, 'media')
-        if not hasattr(cls, 'STATIC_ROOT') or not len(cls.STATIC_ROOT):
-            cls.STATIC_ROOT = path.join(cls.VAR_DIR, 'static')
-        if not hasattr(cls, 'TEMPLATE_DIRS') or not len(cls.TEMPLATE_DIRS):
-            cls.TEMPLATE_DIRS = [path.join(cls.SRC_DIR, 'templates')]
-
-        super(FHSDirsMixin, cls).setup()
+        super(FHSDirsMixin, cnf).setup(*args, **kwargs)
 
 
-# TODO: Delete? django-configurations has Values now.
-#class EnvSettingsMixin(object):
+#class DjangoHostsURLsMixin(object):
+#    """
+#    Standardised configuration for django-hosts. To use it, a configuration
+#    class must set PROJECT_NAME, todolipsum...
 #
+#    This mixin sets the ``ROOT_HOSTCONF`` to ``PROJECT_NAME.hosts``, todolipsum
+#    -
+#    loaded from a PROJECT_NAME.
+#    --
+#    If ``DEBUG`` is True, for each host 'foo.bar' in ``ALLOWED_HOSTS`` add
+#    a host 'foo.bar.local', for easier local development.
+#    """
 #    @classmethod
-#    def setup(cls):
-#        """
-#        Set environment variables listed in ``ENV_SETTINGS`` on this
-#        configuration class.
-#        """
-#        try:
-#            env_vars = cls._get_cinch_meta('ENV_SETTINGS')
-#        except AttributeError:
-#            raise ImproperlyConfigured("EnvSettingsMixin requires a CinchMeta.ENV_SETTINGS iterable.")
-#        for var in env_vars:
-#            setattr(cls, var, environ[var])
-#        super(EnvSettingsMixin, cls).setup()
-
-
-class HostsURLsMixin(object):
-    """
-    Standardised configuration for django-hosts. To use it, a configuration
-    class must set PROJECT_NAME, todolipsum...
-
-    This mixin sets the ``ROOT_HOSTCONF`` to ``PROJECT_NAME.hosts``, todolipsum
-    - 
-    loaded from a PROJECT_NAME.
-    --
-    If ``DEBUG`` is True, for each host 'foo.bar' in ``ALLOWED_HOSTS`` add
-    a host 'foo.bar.local', for easier local development.
-    """
-    @classmethod
-    def setup(cls):
-        # Host routing, with django-hosts
-        # https://django-hosts.readthedocs.org/en/latest/
-        cls.ROOT_HOSTCONF = cls.PROJECT_NAME + '.hosts'
-        cls.DEFAULT_HOST = 'main'
-
-        # Fallback/default url conf
-        cls.ROOT_URLCONF = cls.PROJECT_NAME + '.urls._default'
-
-        # Let project.urls._global include project.urls._debug if it exists
-        if not hasattr(cls, 'DEBUG_URLPATTERNS_ENABLED'):
-            cls.DEBUG_URLPATTERNS_ENABLED = cls.DEBUG
-
-        # Allow 'example.com.local' hosts, if debugging is turned on
-        # TODO: Argh this makes no sense as host validation only works when debug's off.
-        # Okay wait we want this here to work with django-hosts(??)
-        if cls.DEBUG:
-            debug_hosts = []
-            for host in cls.ALLOWED_HOSTS:
-                # TODO: Import something to distinguish ips from named domains.
-                if any(part for part in host.split('.') if not part.isdigit()):
-                    debug_hosts.append(host + '.local')
-
-        super(HostsURLsMixin, cls).setup()
+#    def setup(cnf, *args, **kwargs):
+#        super(DjangoHostsURLsMixin, cnf).setup(*args, **kwargs)
+#
+#        # Host routing with django-hosts - https://django-hosts.readthedocs.org/en/latest/
+#        cls.ROOT_HOSTCONF = cls.PROJECT_NAME + '.hosts'
+#        cls.DEFAULT_HOST = 'main'
+#
+#        # Fallback/default url conf
+#        cls.ROOT_URLCONF = cls.PROJECT_NAME + '.urls._default'
+#
+#        # Let project.urls._global include project.urls._debug if it exists
+#        if not hasattr(cls, 'DEBUG_URLPATTERNS_ENABLED'):
+#            cls.DEBUG_URLPATTERNS_ENABLED = cls.DEBUG
+#
+#        # Allow 'example.com.local' hosts, if debugging is turned on
+#        # TODO: Argh this makes no sense as host validation only works when debug's off.
+#        # Okay wait we want this here to work with django-hosts(??)
+#        if cls.DEBUG:
+#            debug_hosts = []
+#            for host in cls.ALLOWED_HOSTS:
+#                # TODO: Import something to distinguish ips from named domains.
+#                if any(part for part in host.split('.') if not part.isdigit()):
+#                    debug_hosts.append(host + '.local')
+#
+#        super(HostsURLsMixin, cls).setup()
